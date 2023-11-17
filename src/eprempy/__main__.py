356e9b180fa4e3a25cde7f3b0b32dd527421bc6e
Matthew Young <myoung.space.science@gmail.com>
2023-11-17 17:50:55 -0500

    feat: implement parameter CLI
    This allows general users to compare parameter files and generate
    the module and database of default values by calling eprempy
    from the command line

---
+++ src/eprempy/__main__.py
@@ -0,0 +1,97 @@
+import argparse
+
+from .parameter._interface import (
+    run,
+    DESCRIPTIONS,
+    DB_PATH,
+    UserError,
+)
+
+
+if __name__ == '__main__':
+    replacements = {
+        '`src`': 'SRC',
+        '`source`': 'SRC',
+        "`paths`": "the given paths",
+        '`': "'",
+        "the local database": DB_PATH,
+    }
+    parser = argparse.ArgumentParser(
+        prog='eprempy',
+        description="Generate or compare EPREM configuration files.",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    subparsers = parser.add_subparsers(
+        title="modes",
+        dest="mode",
+    )
+    generate_parser = subparsers.add_parser(
+        'generate',
+        description=DESCRIPTIONS['generate'],
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        help="generate database of default parameter values",
+    )
+    generate_parser.add_argument(
+        'source',
+        help="use files in SRC to define default parameter values",
+        metavar='SRC',
+    )
+    generate_parser.add_argument(
+        '--append',
+        help=(
+            "append parameters to the existing database"
+            f"; otherwise, overwrite {DB_PATH}"
+        ),
+        action='store_true',
+    )
+    generate_parser.add_argument(
+        '-v',
+        '--verbose',
+        help="print informational messages",
+        action='store_true',
+    )
+    compare_parser = subparsers.add_parser(
+        'compare',
+        description=DESCRIPTIONS['compare'],
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        help="compare user configuration-file values",
+    )
+    compare_parser.add_argument(
+        '-s',
+        '--source',
+        help=(
+            "use files in SRC when showing default parameter values"
+            f"; otherwise, use definitions in {DB_PATH}"
+        ),
+        metavar='SRC',
+    )
+    compare_parser.add_argument(
+        '-c',
+        '--config',
+        dest='files',
+        help="one or more configuration files to show",
+        nargs='+',
+        metavar=('FILE0', 'FILE1'),
+    )
+    compare_parser.add_argument(
+        '--diff',
+        help=(
+            "show only parameters with differing values"
+            "; otherwise, show all parameters"
+        ),
+        action='store_true',
+    )
+    compare_parser.add_argument(
+        '--names',
+        help="show full file names",
+        action='store_true',
+    )
+    cli = vars(parser.parse_args())
+    usermode = cli.pop('mode')
+    try:
+        run(usermode, cli)
+    except UserError as err:
+        print()
+        print(f"ERROR: {err}")
+        print(f"       Try 'eprempy {usermode} -h'")
+
