3a69d5461f6a67a3f759be28da4fb4fe8188962d
Matthew Young <myoung.space.science@gmail.com>
2023-12-06 16:03:59 -0500

    tests(test_quantity): add test for unitless numeric strings

--- tests/test_quantity/test_functions.py
+++ tests/test_quantity/test_functions.py
@@ -72,6 +72,8 @@ def test_measure(measurables):
     strings = ['1.1', '2.3', 'm']
     expected = quantity.measurement([1.1, 2.3], unit='m')
     assert quantity.measure(strings) == expected
+    expected = quantity.measurement([1.1, 2.3], unit='1')
+    assert quantity.measure(strings[:-1]) == expected
     with pytest.raises(quantity.MeasuringTypeError):
         quantity.measure()
 
