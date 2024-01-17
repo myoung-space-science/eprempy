07f885009cf2d3a3a850c17395ffe81c8e7881a3
Matthew Young <myoung.space.science@gmail.com>
2024-01-17 15:33:34 -0500

    tests: create test_import_all.py
    (cherry picked from commit df834cf8d3463e329ce57c7ec85279b4d27f9b55)

---
+++ tests/test_import_all.py
@@ -0,0 +1,29 @@
+"""
+This module exists to make sure each `__all__` is up to date.
+"""
+
+from eprempy.base import *
+from eprempy.dataset import *
+from eprempy.measurable import *
+from eprempy.measured import *
+from eprempy.metric import *
+from eprempy.numeric import *
+from eprempy.observable import *
+from eprempy.observer import *
+from eprempy.parameter import *
+from eprempy.physical import *
+from eprempy.quantity import *
+from eprempy.real import *
+from eprempy.symbolic import *
+
+
+def test_true():
+    """Unconditionally succeed.
+
+    This "test" is a placeholder. If any of the attempts to `import *` fail,
+    this test will never run. If they all succeed, this test will cause the test
+    runner to indicate that the entire module succeeded.
+    """
+    assert True
+
+
