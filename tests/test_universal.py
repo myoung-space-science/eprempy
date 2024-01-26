1d9e4497fa22d53af02a46394c0639f17ae5ff1e
Matthew Young <myoung.space.science@gmail.com>
2024-01-26 14:03:49 -0500

    tests: update test_universal.py

--- tests/test_universal.py
+++ tests/test_universal.py
@@ -1,5 +1,3 @@
-import pytest
-
 from eprempy import universal
 from eprempy import physical
 
@@ -26,27 +24,4 @@ def test_constant_asscalar():
             s = c.asscalar
             assert isinstance(s, physical.Scalar)
             assert float(c) == float(s)
-
-
-def test_elements():
-    """Test the function that translates mass and charge to element symbol."""
-    # TODO: This needn't test every possible charge state, and testing every
-    # known element may be overkill, but it should extend beyond H and He.
-    cases = [
-        {'in': [1, 0], 'out': ['H']},
-        {'in': [[1], [0]], 'out': ['H']},
-        {'in': [1, +1], 'out': ['H+']},
-        {'in': [1, -1], 'out': ['H-']},
-        {'in': [4, 0], 'out': ['He']},
-        {'in': [4, +1], 'out': ['He+']},
-        {'in': [4, +2], 'out': ['He++']},
-        {'in': [[1, 4], [+1, +2]], 'out': ['H+', 'He++']},
-    ]
-    for case in cases:
-        assert universal.elements(*case['in']) == case['out']
-    with pytest.raises(TypeError):
-        universal.elements([1], [2, 3])
-    with pytest.raises(universal.MassValueError):
-        universal.elements([2], [0])
-
 
