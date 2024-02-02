e04e91f5af44e13b0592f7a8ddb94f0480973212
Matthew Young <myoung.space.science@gmail.com>
2024-02-02 13:52:57 -0500

    refactor: update package __main__.py
    * Remove database mode
    * Add -g/--generate option to parameters mode
    * Use cli.py instead of parameter.py

--- src/eprempy/__main__.py
+++ src/eprempy/__main__.py
@@ -1,11 +1,7 @@
 import argparse
 
-from .parameter._interface import (
-    run,
-    DESCRIPTIONS,
-    DB_PATH,
-    UserError,
-)
+from . import cli
+from . import parameter
 
 
 def print_version():
@@ -14,13 +10,6 @@ def print_version():
 
 
 if __name__ == '__main__':
-    replacements = {
-        '`src`': 'SRC',
-        '`source`': 'SRC',
-        "`paths`": "the given paths",
-        '`': "'",
-        "the local database": DB_PATH,
-    }
     parser = argparse.ArgumentParser(
         prog='eprempy',
         description="Generate or compare EPREM configuration files.",
@@ -35,71 +24,57 @@ if __name__ == '__main__':
         title="modes",
         dest="mode",
     )
-    database_parser = subparsers.add_parser(
-        'database',
-        description=DESCRIPTIONS['database'],
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        help="generate database of default parameter values",
-    )
-    database_parser.add_argument(
-        '-s', '--source',
-        help="use files in SRC to define default parameter values",
-        metavar='SRC',
-    )
-    database_parser.add_argument(
-        '--append',
-        help=(
-            "append parameters to the existing database"
-            f"; otherwise, overwrite {DB_PATH}"
-        ),
-        action='store_true',
-    )
-    database_parser.add_argument(
-        '-v', '--verbose',
-        help="print informational messages",
-        action='store_true',
-    )
     parameters_parser = subparsers.add_parser(
         'parameters',
-        description=DESCRIPTIONS['parameters'],
+        description=cli.DESCRIPTIONS['parameters'],
         formatter_class=argparse.RawDescriptionHelpFormatter,
         help="compare parameter values",
     )
     parameters_parser.add_argument(
+        '-s', '--source',
+        help=(
+            "use files in SRC for default parameter values"
+            f"; otherwise, use definitions in {parameter.DB_PATH}"
+        ),
+        metavar='SRC',
+    )
+    parameters_mode = parameters_parser.add_mutually_exclusive_group(
+        required=True,
+    )
+    parameters_mode.add_argument(
+        '-g', '--generate',
+        help="create a configuration file from default values",
+        metavar='FILE',
+    )
+    parameters_mode.add_argument(
         '-c', '--compare',
         dest='files',
-        help="one or more configuration files to view",
+        help="compare values in the configuration file(s) to default values",
         nargs='+',
         metavar=('FILE0', 'FILE1'),
     )
     parameters_parser.add_argument(
-        '-s', '--source',
-        help=(
-            "use files in SRC when showing default parameter values"
-            f"; otherwise, use definitions in {DB_PATH}"
-        ),
-        metavar='SRC',
+        '-v', '--verbose',
+        help="(generate) print status messages",
+        action='store_true',
     )
     parameters_parser.add_argument(
         '--diff',
-        help=(
-            "show only parameters with differing values"
-            "; otherwise, show all parameters"
-        ),
+        help="(compare) show only parameters with differing values",
         action='store_true',
     )
     parameters_parser.add_argument(
         '--names',
-        help="show full file names",
+        help="(compare) show full file names",
         action='store_true',
     )
-    cli = vars(parser.parse_args())
-    if cli.pop('version', None):
+    options = vars(parser.parse_args())
+    if options.pop('version', None):
         print_version()
-    elif usermode := cli.pop('mode'):
+    elif usermode := options.pop('mode'):
         try:
-            run(usermode, cli)
-        except UserError as err:
+            cli.run(usermode, options)
+        except cli.UserError as err:
             print()
             print(f"ERROR: {err}")
             print(f"       Try running 'python -m eprempy {usermode} -h'")
